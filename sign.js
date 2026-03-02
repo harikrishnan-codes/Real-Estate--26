79164a32dbf67fe48715a417c00af5c47dc77cfc
HariKrishnan <krish96882@gmail.com>
2026-03-02 15:33:46 +0530

    Add files via upload

--- sign.js
+++ sign.js
@@ -34,3 +34,23 @@ function validateForm(event, type) {
 
     return false;
 }
+
+
+
+
+
+
+
+function togglePassword(inputId, icon) {
+    const input = document.getElementById(inputId);
+
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
